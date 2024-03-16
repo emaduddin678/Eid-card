831ab26a0090bcbccfb7c88ebc5f1b99305432f0
emaduddin678 <emaduddin678@gmail.com>
2024-03-16 12:25:24 +0600

    recipent page done

--- src/pages/RecipentForm/RecipentForm.jsx
+++ src/pages/RecipentForm/RecipentForm.jsx
@@ -8,6 +8,7 @@ import ringbox from "../../assets/ringbox.png";
 import globe from "../../assets/globe.png";
 import cone from "../../assets/cone.png";
 import Animatebg from "../../components/Animatebg";
+
 const RecipentForm = () => {
   return (
     <div className='min-h-screen h-full w-full bg-[url("/gradient.png")] overflow-hidden '>
@@ -37,35 +38,35 @@ const RecipentForm = () => {
         </header>
         {/* nav of ui end  */}
         {/* content box start  */}
-        <div className="flex flex-col flex-grow justify-between items-center  md:px-20  gap-x-4 sm:gap-y-4  relative h-full py-16">
+        <div className="flex flex-col flex-grow justify-between items-center  md:px-20  gap-x-4 sm:gap-y-4  relative h-full py-16 px-2">
           <div className="flex flex-col justify-center items-center gap-2 leading-10">
-            <p className="px-2 text-4xl leading-[64px] text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#FFEAB7] to-[#c08b06] font-sansita italic">
-              Fill your details
+            <p className="px-2 text-4xl leadinga text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#FFEAB7] to-[#c08b06] font-sansita italic">
+              Enter the name of the recipient
+            </p>
+            <div>
+              <input
+                type="text"
+                id="success"
+                className="bg-[#61006E]  text-white  placeholder-white  text-sm rounded-lg placeholder:text-center block w-full p-2.5 "
+                placeholder="Recipient’s name"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2 leading-10">
+            <p className="px-2 text-4xl  text-center font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#FFEAB7] to-[#c08b06] font-sansita italic">
+              Your relationship with the recipient
             </p>
             <form className="max-w-sm mx-auto">
-              <div className="flex flex-col gap-3">
-                <input
-                  type="text"
-                  id="success"
-                  className="bg-[#61006E]  text-white  placeholder-white  text-sm rounded-lg placeholder:text-center block w-full p-2.5 "
-                  placeholder="Your name"
-                />
-                <input
-                  type="email"
-                  id="error"
-                  className="bg-[#61006E]  text-white placeholder-white text-sm rounded-lg  placeholder:text-center block w-full p-2.5 "
-                  placeholder="Your email ID"
-                />
-
-                <input
-                  type="text"
-                  id="phone-input"
-                  className="bg-[#61006E]  text-white text-sm rounded-lg placeholder:text-white placeholder:text-center block w-full  p-2.5  "
-                  pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                  placeholder="Your mobile no."
-                  required
-                />
-              </div>
+              <select
+                id="countries"
+                className="appearance-none	bg-[#61006E]  text-white text-sm rounded-lg block w-full p-2.5 "
+              >
+                <option defaultValue={""}>-Recipient Relationship- </option>
+                <option value="father">Father</option>
+                <option value="mother">Mother</option>
+                <option value="brother">Brother</option>
+                <option value="sister">Sister</option>
+              </select>
               <button className="text-center mx-auto block px-7 py-1 rounded-xl font-sansita text-base font-bold text-[#450745] bg-gradient-to-br from-[#FFEAB7] to-[#9D740E] mt-7">
                 Next
               </button>
